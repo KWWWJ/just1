3ac08694198a1c776d0a4e609e800bf5acc46489
KWWWJ <141316552+KWWWJ@users.noreply.github.com>
2023-08-29 17:15:03 +0900

    Update Main.java

--- calculator/Main.java
+++ calculator/Main.java
@@ -8,17 +8,17 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		Calculator calc = new Calculator();
 		
-		System.out.println("첫번째 숫자 입력[100000 입력 시 종료]");
+		System.out.println("첫번째 숫자 입력");
 	 	double input1 = scan.nextDouble();
 	 	while(true) {
-	 		if(input1 == 100000) {
+		 	System.out.println("\n계산 연산자 입력[+, -, *, /, %(나머지), ^(제곱)]");
+			String input = scan.next();
+		 	System.out.println("두번째 숫자 입력[100000 입력 시 종료] \n");
+			double input2 = scan.nextDouble();
+			if(input1 == 100000) {
 				System.out.println("종료");
 				break;
 			}
-		 	System.out.println("\n계산 연산자 입력[+, -, *, /, %(나머지), ^(제곱)]");
-			String input = scan.next();
-		 	System.out.println("두번째 숫자 입력 \n");
-			double input2 = scan.nextDouble();
 			double result = calc.calculate(input, input1, input2);
 			System.out.printf("결과 : %.2f",result);
 			input1=result;
