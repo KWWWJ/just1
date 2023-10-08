6235d57c8690d96851c6985f99ebd40cbb37a480
KWWWJ <141316552+KWWWJ@users.noreply.github.com>
2023-10-08 18:11:58 +0900

    feat:c231006

---
+++ classWeb01/src/main/java/c231006/StudentBean.java
@@ -0,0 +1,23 @@
+package c231006;
+
+public class StudentBean {
+	private static final long serialVersionUID = 1934845011;
+	private String name;
+	private int number;
+	
+	public StudentBean(){} //꼭 하나는 필요하다
+	
+	public void setName(String name) { 
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setNumber(int number) { 
+		this.number = number;
+	}
+	public int getNumber() {
+		return number;
+	}
+}
---
+++ classWeb01/src/main/webapp/c231006-jsp/errorPage1.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>not page</title>
+</head>
+<body>
+	<script type="text/javascript">
+		alert('정상적인 접속이 아닙니다.');
+	</script>
+</body>
+</html>
